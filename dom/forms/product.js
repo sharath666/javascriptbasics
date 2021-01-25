45295b4098b9d347179b3c9c3629e04b14542d6a
sharath <sharathnagalikar@anzaservicesllp.com>
2021-01-25 08:44:11 +0530

    the complete dom events dom structure has been completed

---
+++ dom/forms/product.js
@@ -0,0 +1,7 @@
+let products = [
+    {id:1, name:"redmi"},
+    {id:2, name:"oneplus"},
+    {id:3, name:"iphone"}
+]
+
+console.log(typeof products)
---
+++ dom/forms/users.js
@@ -0,0 +1,10 @@
+const users = [
+    {id:1, name:'sharath'},
+    {id:2, name:'sachin'},
+    {id:3, name:'vinesh'}
+
+
+]
+
+json_users = JSON.stringify(users)
+console.log(json_users)
